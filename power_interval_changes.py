82a9d0ddcc22212950e98f6884e640350e9406fb
Matt T <matthew_thorne@yahoo.co.uk>
2024-02-23 17:39:34 +0000

    Adding basic graph for interval changes

---
+++ power_interval_changes.py
@@ -0,0 +1,49 @@
+from matplotlib import pyplot as plt
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host = "xxx",
+  user = "xxx",
+  passwd = "xxx",
+  database = "xxx"
+)
+
+mycursor = mydb.cursor()
+
+sql = "SELECT date, 60_sec, 5_min, 20_min, 60_min, 90_min FROM t_interval_power_weight WHERE date > CURDATE() - INTERVAL 18 MONTH"
+
+mycursor.execute(sql)
+results = list(mycursor.fetchall())
+
+date = []
+sixty_sec = []
+five_min = []
+twenty_min = []
+sixty_min = []
+ninety_min = []
+
+for i in results:
+    date.append(i[0])
+    sixty_sec.append(i[1])
+    five_min.append(i[2])
+    twenty_min.append(i[3])
+    sixty_min.append(i[4])
+    ninety_min.append(i[5])
+  
+    
+plt.figure(figsize = (20, 30))
+
+# Plotting all the intervals
+plt.plot(date, sixty_sec, color='r', label='60s') 
+plt.plot(date, five_min, color='g', label='5m')
+plt.plot(date, twenty_min, color='b', label='20m') 
+plt.plot(date, sixty_min, color='y', label='60m') 
+plt.plot(date, ninety_min, color='r', label='90m') 
+  
+# Naming x-axis, y-axis
+plt.xlabel("Month") 
+plt.ylabel("W/kg") 
+
+plt.legend() 
+
+plt.show()
