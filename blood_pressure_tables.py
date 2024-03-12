e34af981a2fd60f3310ee94a55b66b5684f9da2c
Matt T <matthew_thorne@yahoo.co.uk>
2024-03-12 19:33:24 +0000

    changing display method

--- blood_pressure_tables.py
+++ blood_pressure_tables.py
@@ -1,7 +1,7 @@
 import mysql.connector
 from matplotlib import pyplot as plt
 import pandas as pd
-import df2img
+
 
 
 mydb = mysql.connector.connect(
@@ -21,15 +21,4 @@ results = list(mycursor.fetchall())
 df = pd.DataFrame(results)
 df.columns =['Date','HR per min', 'Diastolic/Systolic', 'Notes']
 
-fig = df2img.plot_dataframe(
-    df,
-    print_index=False,
-    row_fill_color=("white", "lightgrey"),
-    tbl_header=dict(
-        align="center",
-        fill_color="darkgrey",
-        font_color="black",
-        font_size=14
-    ),
-    col_width=[3, 2, 3, 9]
-)
+display(df)
--- power_interval_table.py
+++ power_interval_table.py
@@ -1,7 +1,6 @@
 import mysql.connector
 from matplotlib import pyplot as plt
 import pandas as pd
-import df2img
 
 mydb = mysql.connector.connect(
   host = "XXXX",
@@ -20,15 +19,4 @@ results = list(mycursor.fetchall())
 df = pd.DataFrame(results)
 df.columns =['Date','60 Second', '5 Min', '20 Min', '60 Min', '90 Min']
 
-fig = df2img.plot_dataframe(
-    df,
-    print_index=False,
-    row_fill_color=("white", "lightgrey"),
-    tbl_header=dict(
-        align="center",
-        fill_color="darkgrey",
-        font_color="black",
-        font_size=14
-    ),
-    col_width=[10, 5, 5, 5, 5, 5]
-)
+display(df)
