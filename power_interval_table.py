be7080c2cd87f18a05086a8bf67853e736ad9ed7
Matt Thorne <matt.thorne1982@gmail.com>
2024-11-03 10:09:19 +0000

    Moving to postgres

--- power_interval_table.py
+++ power_interval_table.py
@@ -1,22 +1,49 @@
-import mysql.connector
+import psycopg2
 from matplotlib import pyplot as plt
 import pandas as pd
+import df2img
 
-mydb = mysql.connector.connect(
-  host = "XXXX",
-  user = "XXXX",
-  passwd = "XXXX",
-  database = "XXXX"
+connection = psycopg2.connect(
+  host = "xxx",
+  port = "xxx",
+  user = "read_only",
+  password = "read_only",
+  database = "Bike_goal"
 )
 
-mycursor = mydb.cursor()
+cursor = connection.cursor()
 
-sql = "SELECT date, 60_sec, 5_min, 20_min, 60_min, 90_min FROM t_interval_power_weight WHERE date > CURDATE() - INTERVAL 18 MONTH"
+sql = """
+SELECT 
+	date
+	, "1_min"
+	, "5_min"
+	, "20_min"
+	, "60_min"
+	, "90_min"
+FROM 
+	bikes.t_interval_power_weight 
+WHERE 
+	date > CURRENT_DATE - INTERVAL '24 MONTH'
+ORDER BY
+    date DESC
+"""
 
-mycursor.execute(sql)
-results = list(mycursor.fetchall())
+cursor.execute(sql)
+results = list(cursor.fetchall())
 
 df = pd.DataFrame(results)
 df.columns =['Date','60 Second', '5 Min', '20 Min', '60 Min', '90 Min']
 
-display(df)
+fig = df2img.plot_dataframe(
+    df,
+    print_index=False,
+    row_fill_color=("white", "lightgrey"),
+    tbl_header=dict(
+        align="center",
+        fill_color="darkgrey",
+        font_color="black",
+        font_size=14
+    ),
+    col_width=[10, 5, 5, 5, 5, 5]
+)
